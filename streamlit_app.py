7a430284967f151b2472ce0fea565ed55c33315d
shaunjohann <114366633+shaunjohann@users.noreply.github.com>
2023-03-01 16:35:27 -0500

    step0.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,40 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import os
+import yaml
+
+from langchain.agents import create_openapi_agent
+from langchain.agents.agent_toolkits import OpenAPIToolkit
+from langchain.llms.openai import OpenAI
+from langchain.requests import RequestsWrapper
+from langchain.tools.json.tool import JsonSpec
+
+with open("openai_openapi.yml") as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
+json_spec=JsonSpec(dict_=data, max_value_length=4000)
+
+headers = {
+    "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}"
+}
+requests_wrapper=RequestsWrapper(headers=headers)
+
+openapi_toolkit = OpenAPIToolkit.from_llm(OpenAI(temperature=0), json_spec, requests_wrapper, verbose=True)
+openapi_agent_executor = create_openapi_agent(
+    llm=OpenAI(temperature=0),
+    toolkit=openapi_toolkit,
+    verbose=True
+)
 
 """
 # Welcome to Streamlit!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
-
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
@@ -36,3 +58,15 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+
+    # Display Langchain commands and allow users to communicate with the agent
+    commands = openapi_agent_executor.get_available_commands()
+    st.write("Available commands:")
+    for command in commands:
+        st.write(f"- {command}")
+
+    user_input = st.text_input("Enter a command:")
+    if user_input:
+        response = openapi_agent_executor.run_command(user_input)
+        st.write("Response:")
+        st.write(response)
